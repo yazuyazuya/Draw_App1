10e0e499f2cd311990230e31d930891f4ee9ef38
yazuyazuya <kzyono-1999@docomo.ne.jp>
2018-11-20 15:48:08 +0900

    Initial Commit

---
+++ App_test6Tests/App_test6Tests.swift
@@ -0,0 +1,34 @@
+//
+//  App_test6Tests.swift
+//  App_test6Tests
+//
+//  Created by 大野和也 on 2018/11/20.
+//  Copyright © 2018 yazuyazuya. All rights reserved.
+//
+
+import XCTest
+@testable import App_test6
+
+class App_test6Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
